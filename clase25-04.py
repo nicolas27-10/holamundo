0d8f8c24dca33a8b1566089ebc342f30317ab79a
Nicolás Alejandro Castillo Cerda <131812087+nicolas27-10@users.noreply.github.com>
2023-04-25 16:29:59 -0400

    se modifico error de calculo

--- clase25-04.py
+++ clase25-04.py
@@ -2,8 +2,8 @@ n=int(input ("ingrese un numero entero: "))
 
 while n >=1:
     for i in range (1,n):
-        if n%2 ==0:
+        if i%2 ==0:
             print (f"el nunmero {i} es par")
-        elif n%2 !=0:
+        elif i%2 !=0:
             print (f"el nunmero {i} es impar")
     break
